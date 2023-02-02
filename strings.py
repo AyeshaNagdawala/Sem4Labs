13a02ae2e97b084fd181d62a0e4e0d5a1ce50877
Ayesha Nagdawala <111572744+AyeshaNagdawala@users.noreply.github.com>
2023-02-02 23:27:08 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,30 @@
+
+# STRING
+string1="Hello Ayesha here."
+print(type(string1))
+a=string1.lower()
+print(a)
+print(string1)
+b=string1.center(50)
+print(b)
+c=string1.split()
+print(c)
+e="  This is Ayesha  "
+d=e.strip()
+print(d)
+
+f=e.casefold()
+g=e.lower()
+print(f)
+print(g)
+
+h=e.count("s")
+print(h)
+
+i="Pokémon"
+j=i.encode()
+print(j)
+
+k="I hate my life."
+l=k.replace("hate","love")
+print(l)
